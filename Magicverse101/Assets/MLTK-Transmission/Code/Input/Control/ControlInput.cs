3f415be59ffca536acee5460ec6666d2181071f2
Abhijit Lothe <alothe@magicleap.com>
2020-03-15 17:51:35 -0400

    Modifying all headers and adding notices file

--- Magicverse101/Assets/MLTK-Transmission/Code/Input/Control/ControlInput.cs
+++ Magicverse101/Assets/MLTK-Transmission/Code/Input/Control/ControlInput.cs
@@ -1,11 +1,14 @@
+// %BANNER_BEGIN%
 // ---------------------------------------------------------------------
+// %COPYRIGHT_BEGIN%
 //
-// Copyright (c) 2019 Magic Leap, Inc. All Rights Reserved.
-// Use of this file is governed by the Creator Agreement, located
-// here: https://id.magicleap.com/creator-terms
+// Copyright (c) 2019-present, Magic Leap, Inc. All Rights Reserved.
+// Use of this file is governed by the Developer Agreement, located
+// here: https://auth.magicleap.com/terms/developer
 //
+// %COPYRIGHT_END%
 // ---------------------------------------------------------------------
-
+// %BANNER_END%
 using System.Collections;
 using UnityEngine;
 using UnityEngine.Events;
